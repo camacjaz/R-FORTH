40e752ac0454b167721f5c081da023c4f472e18c
Jazmin Camacho <Camacjaz16@gmail.com>
2024-01-21 20:23:03 +0000

    commit

--- WarmUp_1/hello_world.c
+++ WarmUp_1/hello_world.c
@@ -1,5 +1,5 @@
 #include <stdio.h> // Include standard input/output file
-int main() { // initialize gtmain function
+int main() { // initialize gtgit main function
    printf("Hello, World!"); // printf() displays the string inside quotation
    return 0; // Exit status
 }
