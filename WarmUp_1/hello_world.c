64d548a1d80d2eba9b41b2cbcc1591c6aca087f0
camacjaz <64205381+camacjaz@users.noreply.github.com>
2024-02-13 11:32:31 -0600

    Delete WarmUp_1 directory

--- WarmUp_1/hello_world.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h> // Include standard input/output file
-int main() { // initialize gtgit git stmain function
-   printf("Hello, World!"); // printf() displays the string inside quotation
-   return 0; // Exit status
-}
