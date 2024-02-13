f535427cbc878cdbc2d81c6040a9e1496c8140f1
camacjaz <64205381+camacjaz@users.noreply.github.com>
2024-02-13 11:54:31 -0600

    Delete gtest-demo directory

--- gtest-demo/countbits.h
+++
@@ -1,5 +0,0 @@
-#ifndef _COUNTBITS_H_
-
-extern int count_bits(unsigned long n);
-
-#endif
--- gtest-demo/square_root_app.c
+++
@@ -1,9 +0,0 @@
-/* standalone app not requiring tests to run */
-
-#include <stdio.h>
-
-#include "square_root.h"
-
-int main(int argc, char* argv[]) {
-    printf("sqrt(2) = %.5f\n", square_root(2.0));
-}
